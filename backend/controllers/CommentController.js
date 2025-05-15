bb2efb8bf1bdf181994c311e4cc583f106c5e36e
didac.medina <didac.medina@inslapineda.cat>
2025-05-15 12:10:56 +0200

    asd

--- backend/controllers/CommentController.js
+++ backend/controllers/CommentController.js
@@ -3,36 +3,33 @@ import Post from "../models/Post.js"
 import User from "../models/User.js"
 
 export const createComment = async (req, res) => {
-  const { post_id, comment, parent } = req.body;
-  const user_id = req.user.id;
-
-  try {
-    let post = await Post.findById(post_id);
-    if (!post) throw new Error("Post not found");
+    const {post_id, comment} = req.body
+    const user_id = req.user.id
     
-    let user = await User.findById(user_id);
-    if (!user) throw new Error("User not found");
+    try {
 
-    const submitedComment = await Comment.create({
-      text: comment,
-      user: user_id,
-      post: post_id,
-      parent: parent || null,
-    });
+        let post = await Post.findById(post_id)
+        if(!post){throw new Error("Post not found")}
+        let user = await User.findById(user_id)
+        if(!user){throw new Error("User not found")}
 
-    // Añadir comentario al post y usuario
-    post.comments.push(submitedComment._id);
-    user.comments.push(submitedComment._id);
+        
+        const submitedComment = await Comment.create({
+            text: comment,
+            user: user_id
+        })
 
-    await post.save();
-    await user.save();
+        post.comments.push(submitedComment._id)
+        user.comments.push(submitedComment._id)
 
-    res.json({ message: "Comment successfully posted!", comment: submitedComment });
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-};
+        await post.save()
+        await user.save()
 
+        res.json({ message: "Comment successfully posted!"});
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
 
 export const getCommentsByPost = async (req, res) => {
   const { id } = req.params;
